13bb7f9e36c58aff4c60f9805216c387e2517993
Oleksiy Pastukhov <oleksiy.pastukhov@senseteq.io>
2022-01-20 12:50:09 +0200

    v0.1.0 release

---
+++ src/components/BoilerplateLayout/components/MainMenu/MainMenu.js
@@ -0,0 +1,58 @@
+import {
+  AppstoreOutlined,
+  DatabaseOutlined,
+  FormatPainterOutlined,
+  TeamOutlined
+} from '@ant-design/icons'
+import { Col, Menu, MenuItem, Row } from '@qonsoll/react-design'
+
+import { LanguageSelect } from 'domains/Translation/components'
+import { useMemo } from 'react'
+import { useTranslations } from 'contexts/Translation'
+
+const MainMenu = () => {
+  const { t } = useTranslations()
+  const menuItems = useMemo(
+    () => [
+      {
+        value: 'DASHBOARD',
+        icon: <AppstoreOutlined />,
+        text: t('Dashboard')
+      },
+      {
+        value: 'ROLES',
+        icon: <TeamOutlined />,
+        text: t('Roles')
+      },
+      {
+        value: 'STYLING',
+        icon: <FormatPainterOutlined />,
+        text: t('Styling')
+      },
+      {
+        value: 'DB_STRUCTURE',
+        icon: <DatabaseOutlined />,
+        text: t('DB structure')
+      }
+    ],
+    [t]
+  )
+  return (
+    <>
+      <Row mb={16}>
+        <Col>
+          <LanguageSelect />
+        </Col>
+      </Row>
+      <Menu mode="inline">
+        {menuItems.map((item, index) => (
+          <MenuItem key={`${item.value}-${index}`} icon={item.icon}>
+            {item.text}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  )
+}
+
+export default MainMenu
